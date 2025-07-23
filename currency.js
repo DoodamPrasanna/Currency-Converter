d5bc9b60bc815cfa52aa6521c3370ae539e5e733
Doodam Sai Prasanna <saiprasannadoodam@gmail.com>
2025-07-23 10:31:58 +0530

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,243 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+
+
+const URL = "https://api.frankfurter.app/latest?amount=1&from=USD&to=INR";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+console.log(dropdowns);
+
+
+const btn = document.querySelector(".Ex-btn");
+console.log(btn);
+
+const fromCurr = document.querySelector(".from select");
+console.log(fromCurr);
+
+const toCurr = document.querySelector(".to select");
+console.log(toCurr);
+
+const msg = document.querySelector(".msg");
+console.log(msg);
+
+
+for(let select of dropdowns){
+  for(currCode in countryList){
+    let newOption = document.createElement("option");
+    newOption.innerText=currCode;
+    newOption.value=currCode;
+    if(select.name ==="from" && currCode === "USD"){
+      newOption.selected= "selected";
+    }
+    else if(select.name ==="To" && currCode === "INR"){
+      newOption.selected= "selected";
+    }
+    select.append(newOption);
+}
+
+select.addEventListener("change", (evt)=>{
+  updateFlag(evt.target);
+});
+}
+
+
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtval = amount.value;
+    if (amtval === "" || amtval < 1) {
+        amtval = 1;
+        amount.value = "1";
+    }
+    const from = fromCurr.value;
+    const to = toCurr.value;
+    const newURL = `https://api.frankfurter.app/latest?amount=${amtval}&from=${from}&to=${to}`;
+    let response = await fetch(newURL);
+    let data = await response.json();
+    let finalAmount = data.rates[to];
+    if (finalAmount === undefined) {
+        msg.innerText = "Conversion failed. Please check currency codes.";
+    } else {
+         msg.innerText = `${amtval} ${from} = ${finalAmount} ${to}`;
+
+    }
+};
+
+
+
+const updateFlag = (element) =>{
+      let currCode = element.value;
+      let countryCode = countryList[currCode];
+      let newsrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+      let img = element.parentElement.querySelector("img");
+      img.src = newsrc;
+      console.log(countryCode);
+};
+
+btn.addEventListener("click", (evt)=>{
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", ()=>{
+  updateExchangeRate();
+});
